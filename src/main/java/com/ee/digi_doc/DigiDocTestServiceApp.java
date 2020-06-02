a86e537af70d0d89c829d9ed7c414ebb493326cf
serhii.matviichuk <serhii.matviichuk@introduct.tech>
2020-06-02 12:54:06 +0300

    Added base project structure.

---
+++ src/main/java/com/ee/digi_doc/DigiDocTestServiceApp.java
@@ -0,0 +1,13 @@
+package com.ee.digi_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiDocTestServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigiDocTestServiceApp.class, args);
+    }
+
+}
